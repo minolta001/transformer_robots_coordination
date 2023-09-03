767a1ebf5d3e56e8243a3bf8fb635692d1e34cdc
Li Chen <minolta001@gmail.com>
2023-09-02 23:07:47 -0400

    forward: make dist reward positive

--- coordination/env/husky/primitives/husky_forward.py
+++ coordination/env/husky/primitives/husky_forward.py
@@ -131,13 +131,13 @@ class HuskyForwardEnv(HuskyEnv):
         # distance between husky and center point of the box 
         dist_husky_box = l2_dist(pos_after, box_after)
         # dist_husky_box_reward = 1 / (1 + dist_husky_box) * self._env_config["dist_reward"]
-        dist_husky_box_reward = (-dist_husky_box) * self._env_config["dist_reward"]
+        dist_husky_box_reward = (20 - dist_husky_box) * self._env_config["dist_reward"]
         # distance between box and goal
         dist_box_goal = l2_dist(goal_pos_after, box_after)
-        dist_box_goal_reward = (-dist_box_goal) * self._env_config["dist_reward"]
+        dist_box_goal_reward = (20 - dist_box_goal) * self._env_config["dist_reward"]
         # quat distance between box and goal
         quat_dist_box_goal = cos_dist(box_forward, goal_forward)
-        quat_dist_box_goal_reward = (-quat_dist_box_goal) * self._env_config["quat_reward"]
+        quat_dist_box_goal_reward = (1 - quat_dist_box_goal) * self._env_config["quat_reward"]
 
         ready_dist_husky_box_reward = 0
 
